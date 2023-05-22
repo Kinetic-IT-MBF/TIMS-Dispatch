c0b6074110f33ef416589cba5106815e38eea1c4
Gayani Udawatta <gayaniu@wearekinetic.com>
2023-05-22 12:31:47 +1000

    Adding the test without numeric script for DPC-328

--- DPC-328/Test_DPC-328-SQL.sql
+++ DPC-328/Test_DPC-328-SQL.sql
@@ -1,17 +1,4 @@
 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dim].[Public_Holiday]') AND type in (N'U'))
-DROP TABLE [dim].[Public_Holiday]
-
-
-SET ANSI_NULLS ON
-
-SET QUOTED_IDENTIFIER ON
-
-CREATE TABLE [dim].[Public_Holiday](
-	[datekey] [int] NULL,
-	[state_key] [int] NULL,
-	[holiday] [varchar](100) NULL
-) ON [PRIMARY]
-
+TRUNCATE TABLE  [dim].[Public_Holiday]
 INSERT [dim].[Public_Holiday] ([datekey], [state_key], [holiday]) VALUES ('20180101', '1', N'New Year''s Day')
 INSERT [dim].[Public_Holiday] ([datekey], [state_key], [holiday]) VALUES ('20180126', '1', N'Australia Day')
