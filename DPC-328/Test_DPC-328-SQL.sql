046fb79d772ed38184d91192430e1eb977e249f9
Gayani Udawatta <gayaniu@wearekinetic.com>
2023-05-22 12:42:00 +1000

    Adding the test script witout blanks for DPC-328

--- DPC-328/Test_DPC-328-SQL.sql
+++ DPC-328/Test_DPC-328-SQL.sql
@@ -1,4 +1,3 @@
-
 TRUNCATE TABLE  [dim].[Public_Holiday]
 INSERT [dim].[Public_Holiday] ([datekey], [state_key], [holiday]) VALUES ('20180101', '1', N'New Year''s Day')
 INSERT [dim].[Public_Holiday] ([datekey], [state_key], [holiday]) VALUES ('20180126', '1', N'Australia Day')
